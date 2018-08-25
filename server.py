40ebb5ed7d611b0469670f33b57ebfd3f6337730
Kevin Kamto <kamtokevin@gmail.com>
2018-08-25 15:18:21 -0400

    feature: basic server

---
+++ server.py
@@ -0,0 +1,24 @@
+import socket
+
+host, port = '', 1999
+
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+listen_socket.bind((host, port))
+listen_socket.listen(1)
+
+print "Hello posting to %s " % port
+
+while True:
+    client_connection, client_address = listen_socket.accept()
+    request = client_connection.recv(1024)
+    print request
+
+    http_response = """\
+HTTP/1.1 200 OK 
+
+hello, world.
+"""
+
+client_connection.sendall(http_response)
+client_connection.close()
