c2996ce9cb1fd044ee2aa152b731f46b7ec45b7d
Kevin Kamto <kamtokevin@gmail.com>
2018-08-27 13:27:21 -0400

    feature: fun code

---
+++ beer.py
@@ -0,0 +1,8 @@
+#lol
+
+bottles = 99
+
+while bottles > 0:
+    print "%s bottles of beer on the wall, \n  %s bottles of beer.  Take one down, pass it around..." % (bottles, bottles)
+    bottles = bottles - 1
+    print "%s bottles of beer on the walll! \n" % bottles
