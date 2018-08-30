ac02c521621bd9699b495b3fd7a84c18249e70a0
Kevin Kamto <kamtokevin@gmail.com>
2018-08-30 11:50:15 -0400

    feature: shell version of beer.py

---
+++ beer.sh
@@ -0,0 +1,11 @@
+echo "How many bottles of beer? Enter a number"
+read num
+
+while [ "$num" -gt 1 ]
+do
+echo "$num bottles of beer on the wall... $num bottles of beer"
+num=$[num-1]
+echo "take one down, pass it around...  $num bottles of beer on the wall!"
+
+done 
+
