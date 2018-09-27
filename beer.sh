a18fe2e36b6ec53bb3fd9aa2825eba997e38bfa6
Kevin Kamto <kamtokevin@gmail.com>
2018-09-27 15:04:39 -0400

    refactor: Object Oriented rewrite of code

--- beer.sh
+++ beer.sh
@@ -1,12 +1,34 @@
-echo "How many bottles of beer? Enter a number"
-read num
+# echo "How many bottles of beer? Enter a number"
+# read num
 
-while [ "$num" -gt 1 ]
-do
-echo "$num bottles of beer on the wall... $num bottles of beer"
-num=$[num-1]
-echo "take one down, pass it around...  $num bottles of beer on the wall!"
+# while [ "$num" -gt 1 ]
+# do
+# echo "$num bottles of beer on the wall... $num bottles of beer"
+# num=$[num-1]
+# echo "take one down, pass it around...  $num bottles of beer on the wall!"
 
-done 
+# done 
 
 #when you're bored, you code
+
+
+#OO Version
+
+initBottles() {
+    echo "Enter amount of bottles"
+    read num
+    return $num
+}
+
+bottlesCountdown() {
+    val=$1
+    while [ "$val" -gt 1 ]
+    do
+    echo "$val bottles of beer on the wall... $val bottles of beer"
+    val=$[val-1]
+    echo "take one down, pass it around...  $val bottles of beer on the wall!"
+    done
+}
+
+initBottles
+bottlesCountdown $num
