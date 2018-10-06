7dea22790db6c63fc2ff2f3ba7668aaa44829cd0
Kevin Kamto <kamtokevin@gmail.com>
2018-10-06 15:16:41 -0400

    refactor: clean up of python script

--- beer.py
+++ beer.py
@@ -1,8 +1,13 @@
 #lol
 
-bottles = 99
+#bottles = 99
 
-while bottles > 0:
-    print "%s bottles of beer on the wall, \n  %s bottles of beer.  Take one down, pass it around..." % (bottles, bottles)
-    bottles -=1
-    print "%s bottles of beer on the walll! \n" % bottles
+# bottles = input("How many bottles? /n")
+
+# while bottles > 0:
+#     print "%s bottles of beer on the wall, \n  %s bottles of beer.  Take one down, pass it around..." % (bottles, bottles)
+#     bottles -=1
+#     print "%s bottles of beer on the walll! \n" % bottles
+
+
+#OO Version
