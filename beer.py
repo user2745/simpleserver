3799aef86f87adadede177e6823bc1b5d2e7d655
Kevin Kamto <37010601+user2745@users.noreply.github.com>
2019-12-11 15:55:26 -0500

    refactor: removed breaking comments

--- beer.py
+++ beer.py
@@ -2,12 +2,12 @@
 
 #bottles = 99
 
-# bottles = input("How many bottles? /n")
+bottles = input("How many bottles? /n")
 
-# while bottles > 0:
-#     print "%s bottles of beer on the wall, \n  %s bottles of beer.  Take one down, pass it around..." % (bottles, bottles)
-#     bottles -=1
-#     print "%s bottles of beer on the walll! \n" % bottles
+while bottles > 0:
+    print "%s bottles of beer on the wall, \n  %s bottles of beer.  Take one down, pass it around..." % (bottles, bottles)
+    bottles -=1
+    print "%s bottles of beer on the walll! \n" % bottles
 
 
 #OO Version
